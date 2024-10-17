76d42f4b5bfe9ee83f0ddeb5b716e89324230af2
llping <llping@bestsec.cn>
2024-10-17 15:32:06 +0800

    Second commit

--- djangoWebproject/settings.py
+++ djangoWebproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-hn@0pru)_eiqy3%nu1g0&j6oin4(^hgm3ob72j2h(e3zd+nra5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -111,7 +111,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
